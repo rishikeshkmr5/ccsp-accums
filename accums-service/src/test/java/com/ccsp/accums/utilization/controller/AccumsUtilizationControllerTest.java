f679b251ff2e30b80d9b32ce0245fbd987cfe432
NithishNA <nnarayanaperumaln@deloitte.com>
2017-11-02 19:29:01 +0530

    Updated the test cases for AccumsUtilization API - BASPIKES -10

--- accums-service/src/test/java/com/ccsp/accums/utilization/controller/AccumsUtilizationControllerTest.java
+++ accums-service/src/test/java/com/ccsp/accums/utilization/controller/AccumsUtilizationControllerTest.java
@@ -6,6 +6,7 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -14,6 +15,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.ccsp.accums.utilization.dto.AccumsUtilizationDTO;
 import com.ccsp.accums.utilization.service.AccumsUtilizationService;
+
+import javassist.NotFoundException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class AccumsUtilizationControllerTest {
@@ -25,13 +28,25 @@ public class AccumsUtilizationControllerTest {
 	private AccumsUtilizationService utilizationService;
 	
 	@Test
-	public void testFetchAccumsUtilizationByMemberIdAndSubscriberId() throws ParseException {
+	public void testFetchAccumsUtilizationByMemberIdAndSubscriberId() throws ParseException, NotFoundException {
 		String memberId = "A0001";
 		String subscriberId = "S0001";
 		List<AccumsUtilizationDTO> utilizationHistory = new ArrayList<>();
 		AccumsUtilizationDTO utilizationDTO = new AccumsUtilizationDTO();
 		utilizationHistory.add(utilizationDTO);
 		when(utilizationService.getAccumsUtilization(memberId, subscriberId)).thenReturn(utilizationHistory);
-		//Assert.assertEquals();
+		List<AccumsUtilizationDTO> resultList = controller.fetchAccumsUtilizationByMemberIdAndSubscriberId(memberId, subscriberId);
+		for(AccumsUtilizationDTO result : resultList) {
+			Assert.assertEquals(result,utilizationDTO);
+		}
+	}
+	
+	@Test(expected = NotFoundException.class)
+	public void testFetchAccumsUtilizationException() throws ParseException, NotFoundException {
+		String memberId = "A0001";
+		String subscriberId = "S0001";
+		List<AccumsUtilizationDTO> utilizationHistory = new ArrayList<>();
+		when(utilizationService.getAccumsUtilization(memberId, subscriberId)).thenReturn(utilizationHistory);
+		List<AccumsUtilizationDTO> resultList = controller.fetchAccumsUtilizationByMemberIdAndSubscriberId(memberId, subscriberId);
 	}
 }
