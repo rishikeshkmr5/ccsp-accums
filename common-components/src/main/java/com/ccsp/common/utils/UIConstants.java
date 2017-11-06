2c908c5e5267f73d82df7370ca49ae6afc857333
vdmehta36 <vdmehta36@gmail.com>
2017-11-06 16:48:21 +0530

    Changed URI. Added constants for Swagger UI

--- common-components/src/main/java/com/ccsp/common/utils/UIConstants.java
+++ common-components/src/main/java/com/ccsp/common/utils/UIConstants.java
@@ -17,10 +17,10 @@ public class UIConstants {
 	/**
 	 * LEDGER HEADER URI
 	 */
-	public static final String LEDGER_HEADER = "/ledger-header";
+	public static final String LEDGER_HEADER = "/accums-utilization";
 	public static final String LEDGER_ENTRY = "/ledger-entry";
 	public static final String ACCUMS_ENTRY_PERIOD = "/accumsentryperiod";
-	public static final String LEDGER_HEADER_CSV = "/ledger-header-csv-file";
+	public static final String LEDGER_HEADER_CSV = "/accums-utilization-csv";
 	
 	/**
 	 * Accumulation Summary URI
@@ -33,9 +33,30 @@ public class UIConstants {
 	 * Accums Inquiry API
 	 */
 	public static final String ACCUMS_INQUIRY = "/inquiry";
-	public static final String BENEFIT_BALANCE = "/benefitbalance";
+	public static final String BENEFIT_BALANCE = "/accums-inquiry-benefit-balance";
 	public static final String CLAIIMS_ASSOCIATED_TO_ACCUMS = "/claimsRelatedToAccums/{accumType}";
 	public static final String BENEFIT_SPENDING = "/benefitspending";
-	public static final String ACCUMS_UTILIZATION = "/accums-utilization";
-	public static final String ACCUMS_CONSUMPTION = "/accumsconsumption";
+	public static final String ACCUMS_UTILIZATION = "/accums-inquiry-spending-summary";
+	public static final String ACCUMS_CONSUMPTION = "/accums-inquiry-consumption";
+	
+	/**
+	 * Claim Rules
+	 */
+	public static final String CLAIM = "/claimRules";
+	
+	/**
+	* Swagger UI constants
+	*/
+	public static final String API_LEDGER_HEADER_DES="Load Accums";
+	public static final String API_LEDGER_HEADER_TAG="Accums Utilization";
+	public static final String API_LEDGER_HEADER_CREATE="Load Accums";
+	public static final String API_LEDGER_HEADER_CREATE_CSV="Load Accums with CSV";
+	
+	public static final String API_ACCUMS_UTILIZATION_DES="Inquire Accums";
+	public static final String API_ACCUMS_UTILIZATION_TAG="Inquiry Utilization";
+	public static final String API_ACCUMS_UTILIZATION_SPENDING_SUM="Inquire Accums Spending Summary";
+	public static final String API_ACCUMS_UTILIZATION_BENEFIT_BAL="Inquire Accums Benefit Balance";
+	public static final String API_ACCUMS_UTILIZATION_CONSUMPTION="Inquire Accums Consumption";
+	
+	
 }
