365451e13de088bbb0567915259dbfa1c9a7c70e
NithishNA <nnarayanaperumaln@deloitte.com>
2017-11-28 17:26:58 +0530

    clearing the old files

--- src/main/java/com/hcsc/ccsp/accums/common/DbTables.java
+++
@@ -1,8 +0,0 @@
-package com.hcsc.ccsp.accums.common;
-
-public enum DbTables {
-
-    LDGR_HDR,
-    LDGR_ENTRY,
-    LDGR_SUM;
-}
--- src/main/java/com/hcsc/ccsp/accums/service/AccumService.java
+++
@@ -1,9 +0,0 @@
-package com.hcsc.ccsp.accums.service;
-
-import com.hcsc.ccsp.accums.dto.AccumUtilization;
-
-public interface AccumService {
-	
-	void updateAccum(AccumUtilization au);
-
-}
